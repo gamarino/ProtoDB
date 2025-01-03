bafc10612436aa3e48e8532f7afd7840f818a1f4
Gustavo Marino <gamarino@numaes.com>
2025-01-03 14:00:37 -0300

    [WIP] queries

--- proto_db/queries.py
+++ proto_db/queries.py
@@ -5,7 +5,202 @@ from typing import cast
 
 from . import DBCollections
 from .exceptions import ProtoUnexpectedException, ProtoValidationException, ProtoCorruptionException
-from .common import Atom, QueryPlan
+from .common import Atom, QueryPlan, AtomPointer
+from .db_access import ObjectTransaction
+import os
+import concurrent.futures
+
+
+# Executor threads for async operations
+max_workers = (os.cpu_count() or 1) * 5
+executor_pool = concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
+
+class Expression:
+    @staticmethod
+    def compile(expression: list) -> Expression:
+        index = 0
+
+        def collect_expression(local_index: int) -> tuple[Expression, int]:
+            if expression[local_index] == '!':
+                local_index += 1
+                following_expression, local_index = collect_expression(local_index)
+                return NotExpression(following_expression), local_index
+            elif expression[local_index] == '&':
+                local_index += 1
+                first_operand, local_index = collect_expression(local_index)
+                second_operand, local_index = collect_expression(local_index)
+                return AndExpression([first_operand, second_operand]), local_index
+            elif expression[local_index] == '|':
+                local_index += 1
+                first_operand, local_index = collect_expression(local_index)
+                second_operand, local_index = collect_expression(local_index)
+                return OrExpression([first_operand, second_operand]), local_index
+            else:
+                # It should be a plain term
+                term_def = expression[local_index]
+                local_index += 1
+                if len(term_def) < 2:
+                    raise ProtoValidationException(
+                        message=f'Invalid term definition: {term_def}. It should contain at least two operands!'
+                    )
+                operand = Operator.get_operator(term_def[1])
+                if len(term_def) != operand.parameter_count + 1:
+                    raise ProtoValidationException(
+                        message=f'Operand {operand} expect at list {operand.parameter_count} parameters!'
+                    )
+                return Term(term_def[0], operand, term_def[2]), local_index
+
+        default_and_expression = list()
+        while index < len(expression):
+            new_expression, index = collect_expression(index)
+            default_and_expression.append(new_expression)
+
+        if len(default_and_expression) >= 2:
+            return AndExpression(default_and_expression)
+        else:
+            return default_and_expression[0]
+
+    def filter_by_alias(self, alias: str):
+        if isinstance(self, Term):
+            atribute_alias = self.target_attribute.split('.', maxsplit=1)[0]
+            if atribute_alias == alias:
+                return self
+            else:
+                return TrueTerm()
+        elif isinstance(self, AndExpression):
+            new_operands: list[Expression] = list()
+            for operand in self.terms:
+                new_operands.append(operand.filter_by_alias(alias))
+            return AndExpression(new_operands)
+        elif isinstance(self, OrExpression):
+            return TrueTerm
+        elif isinstance(self, NotExpression):
+            return NotExpression(self.negated_expression.filter_by_alias(alias))
+        else:
+            raise ProtoValidationException(
+                message=f"It's not possible to filter {self} expression by alias!"
+            )
+
+class AndExpression(Expression):
+    terms: list[Expression]
+
+    def __init__(self, terms: list[Expression]):
+        self.terms = terms
+
+
+class OrExpression(Expression):
+    terms: list[Expression]
+
+    def __init__(self, terms: list[Expression]):
+        self.terms = terms
+
+
+class NotExpression(Expression):
+    negated_expression: Expression
+
+    def __init__(self, negated_expression: Expression):
+        self.negated_expression = negated_expression
+
+
+class Operator:
+    parameter_count: int = 2
+
+    @staticmethod
+    def get_operator(string: str):
+        if string == '==':
+            return Equal()
+        elif string == '!=':
+            return NotEqual()
+        elif string == '>':
+            return Greater()
+        elif string == '>=':
+            return GreaterOrEqual()
+        elif string == '<':
+            return Lower()
+        elif string == '<=':
+            return LowerOrEqual()
+        elif string == 'contains':
+            return Contains()
+        elif string == 'in':
+            return In()
+        elif string == '?T':
+            return IsTrue()
+        elif string == '?!T':
+            return NotTrue()
+        elif string == '?N':
+            return IsNone()
+        elif string == '?!N':
+            return NotNone()
+        else:
+            raise ProtoValidationException(
+                message=f'Unknown operator: {string}!'
+            )
+
+
+class Equal(Operator):
+    parameter_count: int = 2
+
+
+class NotEqual(Operator):
+    parameter_count: int = 2
+
+
+class Greater(Operator):
+    parameter_count: int = 2
+
+
+class GreaterOrEqual(Operator):
+    parameter_count: int = 2
+
+
+class Lower(Operator):
+    parameter_count: int = 2
+
+
+class LowerOrEqual(Operator):
+    parameter_count: int = 2
+
+
+class Contains(Operator):
+    parameter_count: int = 2
+
+
+class In(Operator):
+    parameter_count: int = 2
+
+
+class IsTrue(Operator):
+    parameter_count: int = 1
+
+
+class NotTrue(Operator):
+    parameter_count: int = 1
+
+
+class IsNone(Operator):
+    parameter_count: int = 1
+
+
+class NotNone(Operator):
+    parameter_count: int = 1
+
+
+class Term(Expression):
+    target_attribute: str
+    operation: Operator
+    value: object
+
+    def __init__(self, target_attribute: str, operation: Operator, value: object):
+        self.target_attribute = target_attribute
+        self.operation = operation
+        self.value = value
+
+class TrueTerm(Expression):
+    pass
+
+
+class FalseTerm(Expression):
+    pass
 
 
 class ListPlan(QueryPlan):
@@ -16,10 +211,10 @@ class ListPlan(QueryPlan):
 
     def __init__(self,
                  base_list: list,
-                 transaction_id: uuid.UUID = None,
-                 offset: int = 0,
+                 transaction: ObjectTransaction = None,
+                 atom_pointer: AtomPointer = None,
                  **kwargs):
-        super().__init__(transaction_id=transaction_id, offset=offset)
+        super().__init__(transaction=transaction, atom_pointer=atom_pointer, **kwargs)
         self.base_list = base_list
 
     def execute(self) -> list:
@@ -37,35 +232,74 @@ class FromPlan(QueryPlan):
     alias: str
 
     def __init__(self,
-                 based_on: QueryPlan,
                  alias: str,
-                 transaction_id: uuid.UUID = None,
-                 offset: int = 0,
+                 based_on: QueryPlan,
+                 transaction: ObjectTransaction = None,
+                 atom_pointer: AtomPointer = None,
                  **kwargs):
-        super().__init__(transaction_id=transaction_id, offset=offset)
-        self.based_on = based_on
+        super().__init__(based_on=based_on, transaction=transaction, atom_pointer=atom_pointer, **kwargs)
         self.alias = alias
 
     def execute(self) -> list:
         for item in self.based_on.execute():
-            yield item
+            yield from item
 
     def optimize(self, full_plan: QueryPlan) -> QueryPlan:
-        return self.based_on.optimize(full_plan)
+        return FromPlan(
+            alias=self.alias,
+            based_on=self.based_on.optimize(full_plan),
+            transaction=self.transaction
+        )
 
 
 class WherePlan(QueryPlan):
     """
 
     """
-    # TODO
+    filter: Expression
+
+    def __init__(self,
+                 filter: list = None,
+                 based_on: QueryPlan = None,
+                 transaction: ObjectTransaction = None,
+                 atom_pointer: AtomPointer = None,
+                 **kwargs):
+        super().__init__(based_on=based_on, transaction=transaction, atom_pointer=atom_pointer, **kwargs)
+        self.filter = Expression.compile(filter)
+
+    def execute(self) -> list:
+        # TODO
+        pass
+
+    def optimize(self, full_plan: QueryPlan) -> QueryPlan:
+        return WherePlan(
+            filter=self.filter,
+            based_on=self.based_on.optimize(full_plan),
+            transaction=self.transaction
+        )
 
 
 class GroupByPlan(QueryPlan):
     """
 
     """
-    # TODO
+
+    def __init__(self,
+                 based_on: QueryPlan = None,
+                 transaction: ObjectTransaction = None,
+                 atom_pointer: AtomPointer = None,
+                 **kwargs):
+        super().__init__(based_on=based_on, transaction=transaction, atom_pointer=atom_pointer, **kwargs)
+
+    def execute(self) -> list:
+        # TODO
+        pass
+
+    def optimize(self, full_plan: QueryPlan) -> QueryPlan:
+        return GroupByPlan(
+            based_on=self.based_on.optimize(full_plan),
+            transaction=self.transaction
+        )
 
 
 class SelectPlan(QueryPlan):
@@ -74,12 +308,48 @@ class SelectPlan(QueryPlan):
     """
     # TODO
 
+    def __init__(self,
+                 based_on: QueryPlan = None,
+                 transaction: ObjectTransaction = None,
+                 atom_pointer: AtomPointer = None,
+                 **kwargs):
+        super().__init__(based_on=based_on, transaction=transaction, atom_pointer=atom_pointer, **kwargs)
+
+    def execute(self) -> list:
+        # TODO
+        pass
+
+    def optimize(self, full_plan: QueryPlan) -> QueryPlan:
+        # TODO
+        return SelectPlan(
+            based_on=self.based_on.optimize(full_plan),
+            transaction=self.transaction
+        )
+
+
 
 class OrderByPlan(QueryPlan):
     """
 
     """
     # TODO
+    def __init__(self,
+                 based_on: QueryPlan = None,
+                 transaction: ObjectTransaction = None,
+                 atom_pointer: AtomPointer = None,
+                 **kwargs):
+        super().__init__(based_on=based_on, transaction=transaction, atom_pointer=atom_pointer, **kwargs)
+
+    def execute(self) -> list:
+        # TODO
+        pass
+
+    def optimize(self, full_plan: QueryPlan) -> QueryPlan:
+        # TODO
+        return OrderByPlan(
+            based_on=self.based_on.optimize(full_plan),
+            transaction=self.transaction
+        )
 
 
 class HavingPlan(QueryPlan):
@@ -87,6 +357,23 @@ class HavingPlan(QueryPlan):
 
     """
     # TODO
+    def __init__(self,
+                 based_on: QueryPlan = None,
+                 transaction: ObjectTransaction = None,
+                 atom_pointer: AtomPointer = None,
+                 **kwargs):
+        super().__init__(based_on=based_on, transaction=transaction, atom_pointer=atom_pointer, **kwargs)
+
+    def execute(self) -> list:
+        # TODO
+        pass
+
+    def optimize(self, full_plan: QueryPlan) -> QueryPlan:
+        # TODO
+        return HavingPlan(
+            based_on=self.based_on.optimize(full_plan),
+            transaction=self.transaction
+        )
 
 
 class LimitPlan(QueryPlan):
@@ -95,12 +382,48 @@ class LimitPlan(QueryPlan):
     """
     # TODO
 
+    def __init__(self,
+                 based_on: QueryPlan = None,
+                 transaction: ObjectTransaction = None,
+                 atom_pointer: AtomPointer = None,
+                 **kwargs):
+        super().__init__(based_on=based_on, transaction=transaction, atom_pointer=atom_pointer, **kwargs)
+
+    def execute(self) -> list:
+        # TODO
+        pass
+
+    def optimize(self, full_plan: QueryPlan) -> QueryPlan:
+        # TODO
+        return LimitPlan(
+            based_on=self.based_on.optimize(full_plan),
+            transaction=self.transaction
+        )
+
 
 class OffsetPlan(QueryPlan):
     """
 
     """
     # TODO
+
+    def __init__(self,
+                 based_on: QueryPlan = None,
+                 transaction: ObjectTransaction = None,
+                 atom_pointer: AtomPointer = None,
+                 **kwargs):
+        super().__init__(based_on=based_on, transaction=transaction, atom_pointer=atom_pointer, **kwargs)
+
+    def execute(self) -> list:
+        # TODO
+        pass
+
+    def optimize(self, full_plan: QueryPlan) -> QueryPlan:
+        # TODO
+        return OffsetPlan(
+            based_on=self.based_on.optimize(full_plan),
+            transaction=self.transaction
+        )
 
 
 class ExternalJoinPlan(QueryPlan):
@@ -109,12 +432,49 @@ class ExternalJoinPlan(QueryPlan):
     """
     # TODO
 
+    def __init__(self,
+                 based_on: QueryPlan = None,
+                 transaction: ObjectTransaction = None,
+                 atom_pointer: AtomPointer = None,
+                 **kwargs):
+        super().__init__(based_on=based_on, transaction=transaction, atom_pointer=atom_pointer, **kwargs)
+
+    def execute(self) -> list:
+        # TODO
+        pass
+
+    def optimize(self, full_plan: QueryPlan) -> QueryPlan:
+        # TODO
+        return ExternalJoinPlan(
+            based_on=self.based_on.optimize(full_plan),
+            transaction=self.transaction
+        )
+
 
 class JoinPlan(QueryPlan):
     """
 
     """
     # TODO
+
+    def __init__(self,
+                 based_on: QueryPlan = None,
+                 transaction: ObjectTransaction = None,
+                 atom_pointer: AtomPointer = None,
+                 **kwargs):
+        super().__init__(based_on=based_on, transaction=transaction, atom_pointer=atom_pointer, **kwargs)
+
+    def execute(self) -> list:
+        # TODO
+        pass
+
+    def optimize(self, full_plan: QueryPlan) -> QueryPlan:
+        # TODO
+        return JoinPlan(
+            based_on=self.based_on.optimize(full_plan),
+            transaction=self.transaction
+        )
+
 
 
 class LeftPlan(QueryPlan):
@@ -123,6 +483,24 @@ class LeftPlan(QueryPlan):
     """
     # TODO
 
+    def __init__(self,
+                 based_on: QueryPlan = None,
+                 transaction: ObjectTransaction = None,
+                 atom_pointer: AtomPointer = None,
+                 **kwargs):
+        super().__init__(based_on=based_on, transaction=transaction, atom_pointer=atom_pointer, **kwargs)
+
+    def execute(self) -> list:
+        # TODO
+        pass
+
+    def optimize(self, full_plan: QueryPlan) -> QueryPlan:
+        # TODO
+        return LeftPlan(
+            based_on=self.based_on.optimize(full_plan),
+            transaction=self.transaction
+        )
+
 
 class RightPlan(QueryPlan):
     """
@@ -130,10 +508,46 @@ class RightPlan(QueryPlan):
     """
     # TODO
 
+    def __init__(self,
+                 based_on: QueryPlan = None,
+                 transaction: ObjectTransaction = None,
+                 atom_pointer: AtomPointer = None,
+                 **kwargs):
+        super().__init__(based_on=based_on, transaction=transaction, atom_pointer=atom_pointer, **kwargs)
+
+    def execute(self) -> list:
+        # TODO
+        pass
+
+    def optimize(self, full_plan: QueryPlan) -> QueryPlan:
+        # TODO
+        return RightPlan(
+            based_on=self.based_on.optimize(full_plan),
+            transaction=self.transaction
+        )
+
 
 class UnionPlan(QueryPlan):
     """
 
     """
     # TODO
+
+    def __init__(self,
+                 based_on: QueryPlan = None,
+                 transaction: ObjectTransaction = None,
+                 atom_pointer: AtomPointer = None,
+                 **kwargs):
+        super().__init__(based_on=based_on, transaction=transaction, atom_pointer=atom_pointer, **kwargs)
+
+    def execute(self) -> list:
+        # TODO
+        pass
+
+    def optimize(self, full_plan: QueryPlan) -> QueryPlan:
+        # TODO
+        return UnionPlan(
+            based_on=self.based_on.optimize(full_plan),
+            transaction=self.transaction
+        )
 
