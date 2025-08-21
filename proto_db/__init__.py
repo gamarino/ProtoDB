2d5960e86ff0f66486cf6fd51d5235d66e90504b
Gustavo Marino <gamarino@numaes.com>
2025-08-21 16:02:36 -0300

    [FIX] __init__.py : dependency errors

--- proto_db/__init__.py
+++ proto_db/__init__.py
@@ -1,18 +1,27 @@
 # Lightweight package initializer to avoid heavy imports and circular dependencies during testing.
 # Export exceptions into package namespace for modules that import from proto_db import ProtoCorruptionException, etc.
-from .exceptions import (
-    ProtoBaseException, ProtoUserException, ProtoCorruptionException,
-    ProtoValidationException, ProtoNotSupportedException, ProtoNotAuthorizedException
-)
 
 # Optional: expose storage backends if dependencies are available, otherwise keep None to avoid ImportError
-try:
-    from .cloud_file_storage import CloudFileStorage, CloudBlockProvider, S3Client
-    from .cluster_file_storage import ClusterFileStorage
-    from .standalone_file_storage import StandaloneFileStorage
-except Exception:
-    CloudFileStorage = None
-    CloudBlockProvider = None
-    S3Client = None
-    ClusterFileStorage = None
-    StandaloneFileStorage = None
+from .exceptions import ProtoBaseException, ProtoUserException, ProtoCorruptionException, \
+    ProtoValidationException, ProtoNotSupportedException, ProtoNotAuthorizedException
+from . import common
+from . import dictionaries
+from . import exceptions
+from . import lists
+from . import sets
+from .cloud_file_storage import CloudFileStorage, CloudBlockProvider, S3Client
+from .cluster_file_storage import ClusterFileStorage
+from .common import Atom, Literal, DBObject, MutableObject, DBCollections, QueryPlan, ConcurrentOptimized
+from .db_access import ObjectSpace, Database, ObjectTransaction, BytesAtom
+from .fsm import Timer, FSM
+from .hash_dictionaries import HashDictionary
+from .lists import List
+from .dictionaries import Dictionary, RepeatedKeysDictionary
+from .sets import Set
+from .file_block_provider import FileBlockProvider
+from .memory_storage import MemoryStorage
+from .queries import FromPlan, WherePlan, ListPlan, SelectPlan
+from .standalone_file_storage import StandaloneFileStorage
+from .cloud_file_storage import CloudFileStorage, CloudBlockProvider, S3Client
+from .cluster_file_storage import ClusterFileStorage
+from .standalone_file_storage import StandaloneFileStorage
